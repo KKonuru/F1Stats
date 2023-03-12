acae4bf71696c549138bf58474952688e98bab16
KKonuru <kausthubhkonuru@gmail.com>
2023-03-11 22:07:33 -0500

    driver links

--- f1Info.py
+++ f1Info.py
@@ -1,5 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
+import urllib.request
+
+
 def getDrivers():
     url  = "https://www.formula1.com/en/drivers.html"
     data = requests.get(url)
@@ -9,7 +12,21 @@ def getDrivers():
         firstName = x.find_all("span",class_="firstname")
         LastName = x.find_all("span",class_="lastname")
         drivers.append(firstName[0].string+" "+LastName[0].string)
-    return drivers 
+    return drivers
+
+def getImages():
+    url = "https://www.formula1.com/en/drivers.html"
+    data = requests.get(url)
+    images = list()
+    soup = BeautifulSoup(data.text,"html.parser")
+    for x in soup.find_all("a",class_="listing-item--link"):
+        picture = x.find("picture",class_="listing-item--photo")
+        img = picture.find("img")
+        images.append(img["data-src"])
+    return images
+
+images = getImages()
+print(images)
 
 
 
--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 import f1Info
 
 app = Flask(__name__, template_folder='templates')
+app.config['STATIC_FOLDER'] = 'static'
 
 @app.route('/')
 def hello():
     driversNameList = f1Info.getDrivers()
-    return render_template('Home.html',value=driversNameList)
+    driverImages = f1Info.getImages()
+    return render_template('Home.html',value=driversNameList,images=driverImages)
+@app.route('/driver/<name>')
+def driver(name):
+    return render_template('driver.html',driver=name)
 
 if __name__ == '__main__':
     app.run()
